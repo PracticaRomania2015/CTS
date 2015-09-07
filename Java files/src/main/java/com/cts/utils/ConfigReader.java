df4fed9c61f7df9ac856436b31d1bba497747ceb
AndreiSmitko <andrei.smitko@cerner.com>
2015-09-07 12:59:12 +0300

    Made the cfg auto-update itself.
    This should work even if the server does not auto re-boot.

--- Java files/src/main/java/com/cts/utils/ConfigReader.java
+++ Java files/src/main/java/com/cts/utils/ConfigReader.java
@@ -3,6 +3,7 @@
  */
 package com.cts.utils;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -29,15 +30,19 @@ public class ConfigReader {
 	}
 
 	private static ConfigReader instance = null;
-	private Properties prop = null;
-	private InputStream input = null;
-	private String filePath = null;
+	private static Properties prop = null;
+	private static InputStream input = null;
+	private static String filePath = ConfigReader.class.getClassLoader().getResource("config_en.properties").getPath().toString();
+	private static long initialModDate = 0;
 	
 	private static Logger logger = Logger.getLogger(ConfigReader.class.getName());
 	
+	private void CheckForChanges() {
+		return;
+	}
+	
 	protected ConfigReader() {
 		prop = new Properties();
-		filePath = ConfigReader.class.getClassLoader().getResource("config_en.properties").getPath().toString();
 		try {
 			input = new FileInputStream(filePath);
 			prop.load(input);
@@ -50,6 +55,36 @@ public class ConfigReader {
 	public static ConfigReader getInstance() {
 		if(instance == null) {
 			instance = new ConfigReader();
+			Thread checker = new Thread() {
+				@SuppressWarnings("static-access")
+				public void run() {
+					while(true) {
+						try {
+							logger.info("Periodic configuration file check.");
+							long dateMod = new File(filePath).lastModified();
+							if ( dateMod != initialModDate ) {
+								try {
+									input = new FileInputStream(filePath);
+									prop.load(input);
+									logger.info("Config file was updated.");
+								} catch (IOException e) {
+									logger.error("File " + filePath + "not found.");
+									e.printStackTrace();
+								}
+								initialModDate = dateMod;
+							}
+							else {
+								logger.info("Config file was not changed.");
+							}
+							this.sleep(600000);
+						} catch (InterruptedException e) {
+							logger.error("The check and potential reconfiguration failed !");
+							e.printStackTrace();
+						}
+					}
+				}
+			};
+			checker.start();
 	    }
 	    return instance;
 	}
