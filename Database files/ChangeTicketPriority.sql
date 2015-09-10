58672c24003d85110c715d30aea8c4e879c1009e
AntoniuStelea <antoniu.stelea@cerner.com>
2015-09-10 09:29:41 +0300

    Update

--- Database files/ChangeTicketPriority.sql
+++ Database files/ChangeTicketPriority.sql
@@ -1,6 +1,6 @@
 USE [CTS]
 GO
-/****** Object:  StoredProcedure [dbo].[ChangeTicketPriority]    Script Date: 9/9/2015 10:44:44 AM ******/
+/****** Object:  StoredProcedure [dbo].[ChangeTicketPriority]    Script Date: 9/10/2015 9:07:01 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -21,12 +21,16 @@ BEGIN
 	
 	SELECT @Error = 1
 
-	UPDATE Ticket
-	SET PriorityId = @PriorityId, @Error = 0
-	WHERE TicketId = @TicketId
+	SELECT @Error = 0
+	FROM Priority
+	WHERE PriorityId = @PriorityId
 
 	IF (@Error = 0)
 	BEGIN
+		UPDATE Ticket
+		SET PriorityId = @PriorityId
+		WHERE TicketId = @TicketId
+		
 		-- add history event
 		SELECT @Action = 'Change ticket [TicketId = ' + CONVERT(VARCHAR(25), @TicketId, 126) + '] priority to ' + (SELECT PriorityName FROM Priority WHERE PriorityId = @PriorityId)
 
