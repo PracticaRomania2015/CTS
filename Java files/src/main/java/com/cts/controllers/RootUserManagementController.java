50afb23aae7b0c783b91bc4a5aeab0b434580145
AndreiSmitko <andrei.smitko@cerner.com>
2015-09-17 11:00:54 +0300

    Manage user rights WIP_2

--- Java files/src/main/java/com/cts/controllers/RootUserManagementController.java
+++ Java files/src/main/java/com/cts/controllers/RootUserManagementController.java
@@ -10,18 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.cts.communication.CategoryResponse;
 import com.cts.communication.ResponseValues;
-import com.cts.communication.TicketResponse;
-import com.cts.dao.CategoryDAO;
-import com.cts.dao.CategoryDAOInterface;
-import com.cts.dao.TicketDAO;
-import com.cts.dao.TicketDAOInterface;
+import com.cts.communication.UserRightsResponse;
 import com.cts.dao.UserDAO;
 import com.cts.dao.UserDAOInterface;
-import com.cts.entities.Category;
-import com.cts.entities.Ticket;
-import com.cts.entities.ViewTicketsRequest;
+import com.cts.entities.User;
+import com.cts.entities.UserStatus;
+import com.cts.entities.ViewUsersRequest;
 
 public class RootUserManagementController {
 	
@@ -34,21 +29,38 @@ public class RootUserManagementController {
 
 		UserDAOInterface userDAO = new UserDAO();
 		StringBuilder totalNumberOfPages = new StringBuilder();
-		ArrayList<Ticket> users = userDAO.getTickets(viewUserRequest, totalNumberOfPages);
+		ArrayList<User> users = userDAO.getUsers(viewUserRequest, totalNumberOfPages);
 		String usersJson;
 		ObjectMapper objectMapper = new ObjectMapper();
 		try {
-
 			usersJson = objectMapper.writeValueAsString(users);
+			logger.info("The list of users was successfully retrieved!");
+			return "{\"totalNumberOfPages\":" + totalNumberOfPages + ",\"users\":" + usersJson + "}";
 		} catch (IOException e) {
 
 			logger.error("Json error when trying to map the array of users.");
-			return new TicketResponse().getMessageJson(ResponseValues.UNKNOWN); // brain damage
+			return new UserRightsResponse().getMessageJson(ResponseValues.UNKNOWN);
 		}
-		logger.info("The list of users was successfully retrieved!");
-		return "{\"totalNumberOfPages\":" + totalNumberOfPages + ",\"users\":" + usersJson + "}";
 	}
 	
 	// get categories admin status for this user ...
+	@RequestMapping(value = "/getUserAdminStatus", method = RequestMethod.POST)
+
+		
+		//TODO: implement me
+		
+		return null;
+		
+	}
+	
+	// Alter admin rights of user ...
+	@RequestMapping(value = "/setUserAdminStatus", method = RequestMethod.POST)
+
+			
+		//TODO: implement me
+			
+		return null;
+			
+	}
 	
 }
