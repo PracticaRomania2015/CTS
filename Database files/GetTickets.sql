98962b2e0d2987d55a94e23adac98674bd91d0e8
AntoniuStelea <antoniu.stelea@cerner.com>
2015-09-22 12:06:06 +0300

    Improve performance of GetTickets

--- Database files/GetTickets.sql
+++ Database files/GetTickets.sql
@@ -1,11 +1,11 @@
 USE [CTS]
 GO
 
-/****** Object:  StoredProcedure [dbo].[GetTickets]    Script Date: 9/21/2015 12:53:29 PM ******/
+/****** Object:  StoredProcedure [dbo].[GetTickets]    Script Date: 9/22/2015 12:03:52 PM ******/
 DROP PROCEDURE [dbo].[GetTickets]
 GO
 
-/****** Object:  StoredProcedure [dbo].[GetTickets]    Script Date: 9/21/2015 12:53:29 PM ******/
+/****** Object:  StoredProcedure [dbo].[GetTickets]    Script Date: 9/22/2015 12:03:52 PM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -27,7 +27,6 @@ AS
 BEGIN
 	
 	DECLARE @TotalNumberOfTickets INT
-	DECLARE @TicketTo INT
 	DECLARE @Action varchar(1000)
 	DECLARE @DateTime datetime
 
@@ -64,77 +63,45 @@ BEGIN
 		IF @TotalNumberOfPages = 0
 		BEGIN
 			SET @TotalNumberOfPages=1
-		END
+		END;
 
-		SELECT @TicketTo = @RequestedPageNumber * @TicketsPerPage
-
-		IF (@TicketTo > @TotalNumberOfTickets)
-		BEGIN
-			SELECT @TicketsPerPage = @TicketsPerPage - (@TicketTo - @TotalNumberOfTickets)
-		END
-
-		SELECT * FROM (SELECT TOP (@TicketsPerPage) * FROM (SELECT TOP (@TicketTo) Ticket.TicketId AS TicketId, MAX(Ticket.Subject) AS Subject, MIN(TicketComment.DateTime) AS DateTime, MAX(Category.CategoryName) AS CategoryName, MAX(State.StateName) AS StateName, MAX(FirstName) AS FirstName, MAX(LastName) AS LastName, MAX(TicketComment.DateTime) AS LastDateTime, MAX(Priority.PriorityName) as PriorityName
-		FROM Ticket
-		INNER JOIN TicketComment ON Ticket.TicketId = TicketComment.TicketId
-		INNER JOIN State ON Ticket.StateId = State.StateId
-		INNER JOIN Category ON Ticket.CategoryId = Category.CategoryId
-		LEFT JOIN [User] ON [User].UserId = Ticket.AssignedToUserId
-		INNER JOIN Priority ON Ticket.PriorityId = Priority.PriorityId
-		WHERE (@TextToSearch = '' AND TicketComment.UserId = @UserId AND (Ticket.AssignedToUserId != @UserId OR Ticket.AssignedToUserId IS NULL))
-			OR (@SearchType = 'Subject' AND Subject like ('%' + @TextToSearch + '%') AND TicketComment.UserId = @UserId AND (Ticket.AssignedToUserId != @UserId OR Ticket.AssignedToUserId IS NULL))
-			OR (@SearchType = 'Category' AND CategoryName like ('%' + @TextToSearch + '%') AND TicketComment.UserId = @UserId AND (Ticket.AssignedToUserId != @UserId OR Ticket.AssignedToUserId IS NULL))
-			OR (@SearchType = 'Status' AND StateName like ('%' + @TextToSearch + '%') AND TicketComment.UserId = @UserId AND (Ticket.AssignedToUserId != @UserId OR Ticket.AssignedToUserId IS NULL)) 
-		GROUP BY Ticket.TicketId
-		ORDER BY 
-			CASE WHEN @SortType = '' THEN MIN(TicketComment.DateTime) END DESC,
-			CASE WHEN @SortType = 'TicketId' AND @IsSearchASC = 1 THEN Ticket.TicketId END ASC,
-			CASE WHEN @SortType = 'Subject' AND @IsSearchASC = 1 THEN MAX(Subject) END ASC,
-			CASE WHEN @SortType = 'Category' AND @IsSearchASC = 1 THEN MAX(CategoryName) END ASC,
-			CASE WHEN @SortType = 'Status' AND @IsSearchASC = 1 THEN MAX(StateName) END ASC,
-			CASE WHEN @SortType = 'SubmitDate' AND @IsSearchASC = 1 THEN MIN(DateTime) END ASC,
-			CASE WHEN @SortType = 'LastDateTime' AND @IsSearchASC = 1 THEN MAX(DateTime) END ASC,
-			CASE WHEN @SortType = 'Priority' AND @IsSearchASC = 1 THEN MAX(PriorityName) END ASC,
-			CASE WHEN @SortType = 'TicketId' AND @IsSearchASC = 0 THEN Ticket.TicketId END DESC,
-			CASE WHEN @SortType = 'Subject' AND @IsSearchASC = 0 THEN MAX(Subject) END DESC,
-			CASE WHEN @SortType = 'Category' AND @IsSearchASC = 0 THEN MAX(CategoryName) END DESC,
-			CASE WHEN @SortType = 'Status' AND @IsSearchASC = 0 THEN MAX(StateName) END DESC,
-			CASE WHEN @SortType = 'SubmitDate' AND @IsSearchASC = 0 THEN MIN(DateTime) END DESC,
-			CASE WHEN @SortType = 'LastDateTime' AND @IsSearchASC = 0 THEN MAX(DateTime) END DESC,
-			CASE WHEN @SortType = 'Priority' AND @IsSearchASC = 0 THEN MAX(PriorityName) END DESC
-		) t GROUP BY t.TicketId, t.Subject, t.DateTime, t.CategoryName, t.StateName, t.FirstName, t.LastName, t.LastDateTime, t.PriorityName
-			ORDER BY
-				CASE WHEN @SortType = '' THEN MIN(t.DateTime) END ASC,
-				CASE WHEN @SortType = 'TicketId' AND @IsSearchASC = 1 THEN t.TicketId END DESC,
-				CASE WHEN @SortType = 'Subject' AND @IsSearchASC = 1 THEN t.Subject END DESC,
-				CASE WHEN @SortType = 'Category' AND @IsSearchASC = 1 THEN t.CategoryName END DESC,
-				CASE WHEN @SortType = 'Status' AND @IsSearchASC = 1 THEN t.StateName END DESC,
-				CASE WHEN @SortType = 'SubmitDate' AND @IsSearchASC = 1 THEN t.DateTime END DESC,
-				CASE WHEN @SortType = 'LastDateTime' AND @IsSearchASC = 1 THEN t.LastDateTime END DESC,
-				CASE WHEN @SortType = 'Priority' AND @IsSearchASC = 1 THEN PriorityName END DESC,
-				CASE WHEN @SortType = 'TicketId' AND @IsSearchASC = 0 THEN t.TicketId END ASC,
-				CASE WHEN @SortType = 'Subject' AND @IsSearchASC = 0 THEN t.Subject END ASC,
-				CASE WHEN @SortType = 'Category' AND @IsSearchASC = 0 THEN t.CategoryName END ASC,
-				CASE WHEN @SortType = 'Status' AND @IsSearchASC = 0 THEN t.StateName END ASC,
-				CASE WHEN @SortType = 'SubmitDate' AND @IsSearchASC = 0 THEN t.DateTime END ASC,
-				CASE WHEN @SortType = 'LastDateTime' AND @IsSearchASC = 0 THEN t.LastDateTime END ASC,
-				CASE WHEN @SortType = 'Priority' AND @IsSearchASC = 0 THEN PriorityName END ASC
-				) tt GROUP BY tt.TicketId, tt.Subject, tt.DateTime, tt.CategoryName, tt.StateName, tt.FirstName, tt.LastName, tt.LastDateTime, tt.PriorityName
-					 ORDER BY
-						CASE WHEN @SortType = '' THEN MIN(tt.DateTime) END DESC,
-						CASE WHEN @SortType = 'TicketId' AND @IsSearchASC = 1 THEN tt.TicketId END ASC,
-						CASE WHEN @SortType = 'Subject' AND @IsSearchASC = 1 THEN tt.Subject END ASC,
-						CASE WHEN @SortType = 'Category' AND @IsSearchASC = 1 THEN tt.CategoryName END ASC,
-						CASE WHEN @SortType = 'Status' AND @IsSearchASC = 1 THEN tt.StateName END ASC,
-						CASE WHEN @SortType = 'SubmitDate' AND @IsSearchASC = 1 THEN tt.DateTime END ASC,
-						CASE WHEN @SortType = 'LastDateTime' AND @IsSearchASC = 1 THEN tt.LastDateTime END ASC,
-						CASE WHEN @SortType = 'Priority' AND @IsSearchASC = 1 THEN PriorityName END ASC,
-						CASE WHEN @SortType = 'TicketId' AND @IsSearchASC = 0 THEN tt.TicketId END DESC,
-						CASE WHEN @SortType = 'Subject' AND @IsSearchASC = 0 THEN tt.Subject END DESC,
-						CASE WHEN @SortType = 'Category' AND @IsSearchASC = 0 THEN tt.CategoryName END DESC,
-						CASE WHEN @SortType = 'Status' AND @IsSearchASC = 0 THEN tt.StateName END DESC,
-						CASE WHEN @SortType = 'SubmitDate' AND @IsSearchASC = 0 THEN tt.DateTime END DESC,
-						CASE WHEN @SortType = 'LastDateTime' AND @IsSearchASC = 0 THEN tt.LastDateTime END DESC,
-						CASE WHEN @SortType = 'Priority' AND @IsSearchASC = 0 THEN PriorityName END DESC
+		WITH Tickets AS
+		(
+			SELECT Ticket.TicketId, MAX(Ticket.Subject) AS Subject, MIN(TicketComment.DateTime) AS DateTime, MAX(Category.CategoryName) AS CategoryName, MAX(State.StateName) AS StateName, MAX(FirstName) AS FirstName, MAX(LastName) AS LastName, MAX(TicketComment.DateTime) AS LastDateTime, MAX(Priority.PriorityName) as PriorityName,
+			ROW_NUMBER() OVER
+			(
+				ORDER BY 
+					CASE WHEN @SortType = '' THEN MIN(TicketComment.DateTime) END DESC,
+					CASE WHEN @SortType = 'TicketId' AND @IsSearchASC = 1 THEN Ticket.TicketId END ASC,
+					CASE WHEN @SortType = 'Subject' AND @IsSearchASC = 1 THEN MAX(Ticket.Subject) END ASC,
+					CASE WHEN @SortType = 'Category' AND @IsSearchASC = 1 THEN MAX(CategoryName) END ASC,
+					CASE WHEN @SortType = 'Status' AND @IsSearchASC = 1 THEN MAX(StateName) END ASC,
+					CASE WHEN @SortType = 'SubmitDate' AND @IsSearchASC = 1 THEN MIN(TicketComment.DateTime) END ASC,
+					CASE WHEN @SortType = 'LastDateTime' AND @IsSearchASC = 1 THEN MAX(TicketComment.DateTime) END ASC,
+					CASE WHEN @SortType = 'Priority' AND @IsSearchASC = 1 THEN MAX(PriorityName) END ASC,
+					CASE WHEN @SortType = 'TicketId' AND @IsSearchASC = 0 THEN Ticket.TicketId END DESC,
+					CASE WHEN @SortType = 'Subject' AND @IsSearchASC = 0 THEN MAX(Subject) END DESC,
+					CASE WHEN @SortType = 'Category' AND @IsSearchASC = 0 THEN MAX(CategoryName) END DESC,
+					CASE WHEN @SortType = 'Status' AND @IsSearchASC = 0 THEN MAX(StateName) END DESC,
+					CASE WHEN @SortType = 'SubmitDate' AND @IsSearchASC = 0 THEN MIN(TicketComment.DateTime) END DESC,
+					CASE WHEN @SortType = 'LastDateTime' AND @IsSearchASC = 0 THEN MAX(TicketComment.DateTime) END DESC,
+					CASE WHEN @SortType = 'Priority' AND @IsSearchASC = 0 THEN MAX(PriorityName) END DESC
+			) AS RowNumber
+			FROM Ticket
+				INNER JOIN TicketComment ON Ticket.TicketId = TicketComment.TicketId
+				INNER JOIN State ON Ticket.StateId = State.StateId
+				INNER JOIN Category ON Ticket.CategoryId = Category.CategoryId
+				LEFT JOIN [User] ON [User].UserId = Ticket.AssignedToUserId
+				INNER JOIN Priority ON Ticket.PriorityId = Priority.PriorityId
+			WHERE (@TextToSearch = '' AND TicketComment.UserId = @UserId AND (Ticket.AssignedToUserId != @UserId OR Ticket.AssignedToUserId IS NULL))
+				OR (@SearchType = 'Subject' AND Subject like ('%' + @TextToSearch + '%') AND TicketComment.UserId = @UserId AND (Ticket.AssignedToUserId != @UserId OR Ticket.AssignedToUserId IS NULL))
+				OR (@SearchType = 'Category' AND CategoryName like ('%' + @TextToSearch + '%') AND TicketComment.UserId = @UserId AND (Ticket.AssignedToUserId != @UserId OR Ticket.AssignedToUserId IS NULL))
+				OR (@SearchType = 'Status' AND StateName like ('%' + @TextToSearch + '%') AND TicketComment.UserId = @UserId AND (Ticket.AssignedToUserId != @UserId OR Ticket.AssignedToUserId IS NULL))
+			GROUP BY Ticket.TicketId
+		)
+		SELECT TicketId, Subject, DateTime, CategoryName, StateName, FirstName, LastName, LastDateTime, PriorityName
+		FROM Tickets
+		WHERE RowNumber BETWEEN ((@RequestedPageNumber-1)*@TicketsPerPage+1) AND (@RequestedPageNumber*@TicketsPerPage)
 
 		-- set param for audit event
 		SELECT @Action = 'Requested personal tickets; the stored procedure was successfully executed.'
@@ -145,12 +112,12 @@ BEGIN
 		(
 			SELECT TicketComment.TicketId, MAX(Ticket.Subject) AS Subject, MIN(TicketComment.DateTime) AS DateTime, MAX(Category.CategoryName) AS CategoryName, MAX(State.StateName) AS StateName, MAX(FirstName) AS FirstName, MAX(LastName) AS LastName, MAX(TicketComment.DateTime) AS LastDateTime, MAX(Priority.PriorityName) as PriorityName
 			FROM Ticket
-			INNER JOIN TicketComment ON Ticket.TicketId = TicketComment.TicketId
-			INNER JOIN State ON Ticket.StateId = State.StateId
-			INNER JOIN Category ON Ticket.CategoryId = Category.CategoryId
-			INNER JOIN UserCategory ON UserCategory.UserId = @UserId AND (UserCategory.CategoryId = Ticket.CategoryId OR UserCategory.CategoryId = Category.ParentCategoryId)
-			LEFT JOIN [User] ON [User].UserId = Ticket.AssignedToUserId
-			INNER JOIN Priority ON Ticket.PriorityId = Priority.PriorityId
+				INNER JOIN TicketComment ON Ticket.TicketId = TicketComment.TicketId
+				INNER JOIN State ON Ticket.StateId = State.StateId
+				INNER JOIN Category ON Ticket.CategoryId = Category.CategoryId
+				INNER JOIN UserCategory ON UserCategory.UserId = @UserId AND (UserCategory.CategoryId = Ticket.CategoryId OR UserCategory.CategoryId = Category.ParentCategoryId)
+				LEFT JOIN [User] ON [User].UserId = Ticket.AssignedToUserId
+				INNER JOIN Priority ON Ticket.PriorityId = Priority.PriorityId
 			WHERE (@TextToSearch = '')
 				OR (@SearchType = 'Subject' AND Subject like ('%' + @TextToSearch + '%'))
 				OR (@SearchType = 'Category' AND CategoryName like ('%' + @TextToSearch + '%'))
@@ -170,72 +137,46 @@ BEGIN
 		IF @TotalNumberOfPages = 0
 		BEGIN
 			SET @TotalNumberOfPages=1
-		END
+		END;
 
-		SELECT @TicketTo = @RequestedPageNumber * @TicketsPerPage
-
-		IF (@TicketTo > @TotalNumberOfTickets)
-		BEGIN
-			SELECT @TicketsPerPage = @TicketsPerPage - (@TicketTo - @TotalNumberOfTickets)
-		END
-
-		SELECT * FROM (SELECT TOP (@TicketsPerPage) * FROM (SELECT TOP (@TicketTo) Ticket.TicketId, MAX(Ticket.Subject) AS Subject, MIN(TicketComment.DateTime) AS DateTime, MAX(Category.CategoryName) AS CategoryName, MAX(State.StateName) AS StateName, MAX(FirstName) AS FirstName, MAX(LastName) AS LastName, MAX(TicketComment.DateTime) AS LastDateTime, MAX(Priority.PriorityName) as PriorityName
-		FROM Ticket
-		INNER JOIN TicketComment ON Ticket.TicketId = TicketComment.TicketId
-		INNER JOIN State ON Ticket.StateId = State.StateId
-		INNER JOIN Category ON Ticket.CategoryId = Category.CategoryId
-		INNER JOIN UserCategory ON UserCategory.UserId = @UserId AND (UserCategory.CategoryId = Ticket.CategoryId OR UserCategory.CategoryId = Category.ParentCategoryId)
-		LEFT JOIN [User] ON [User].UserId = Ticket.AssignedToUserId
-		INNER JOIN Priority ON Ticket.PriorityId = Priority.PriorityId
-		WHERE (@TextToSearch = '')
-			OR (@SearchType = 'Subject' AND Subject like ('%' + @TextToSearch + '%'))
-			OR (@SearchType = 'Category' AND CategoryName like ('%' + @TextToSearch + '%'))
-			OR (@SearchType = 'Status' AND StateName like ('%' + @TextToSearch + '%')) 
-		GROUP BY Ticket.TicketId
-		ORDER BY 
-			CASE WHEN @SortType = '' THEN MIN(TicketComment.DateTime) END DESC,
-			CASE WHEN @SortType = 'TicketId' AND @IsSearchASC = 1 THEN Ticket.TicketId END ASC,
-			CASE WHEN @SortType = 'Subject' AND @IsSearchASC = 1 THEN MAX(Subject) END ASC,
-			CASE WHEN @SortType = 'Category' AND @IsSearchASC = 1 THEN MAX(CategoryName) END ASC,
-			CASE WHEN @SortType = 'Status' AND @IsSearchASC = 1 THEN MAX(StateName) END ASC,
-			CASE WHEN @SortType = 'SubmitDate' AND @IsSearchASC = 1 THEN MIN(DateTime) END ASC,
-			CASE WHEN @SortType = 'LastDateTime' AND @IsSearchASC = 1 THEN  MAX(DateTime) END ASC,
-			CASE WHEN @SortType = 'TicketId' AND @IsSearchASC = 0 THEN Ticket.TicketId END DESC,
-			CASE WHEN @SortType = 'Subject' AND @IsSearchASC = 0 THEN MAX(Subject) END DESC,
-			CASE WHEN @SortType = 'Category' AND @IsSearchASC = 0 THEN MAX(CategoryName) END DESC,
-			CASE WHEN @SortType = 'Status' AND @IsSearchASC = 0 THEN MAX(StateName) END DESC,
-			CASE WHEN @SortType = 'SubmitDate' AND @IsSearchASC = 0 THEN MIN(DateTime) END DESC,
-			CASE WHEN @SortType = 'LastDateTime' AND @IsSearchASC = 0 THEN MAX(DateTime) END DESC
-		) t GROUP BY t.TicketId, t.Subject, t.DateTime, t.CategoryName, t.StateName, t.FirstName, t.LastName, t.LastDateTime, t.PriorityName
-			ORDER BY
-				CASE WHEN @SortType = '' THEN MIN(t.DateTime) END ASC,
-				CASE WHEN @SortType = 'TicketId' AND @IsSearchASC = 1 THEN t.TicketId END DESC,
-				CASE WHEN @SortType = 'Subject' AND @IsSearchASC = 1 THEN t.Subject END DESC,
-				CASE WHEN @SortType = 'Category' AND @IsSearchASC = 1 THEN t.CategoryName END DESC,
-				CASE WHEN @SortType = 'Status' AND @IsSearchASC = 1 THEN t.StateName END DESC,
-				CASE WHEN @SortType = 'SubmitDate' AND @IsSearchASC = 1 THEN t.DateTime END DESC,
-				CASE WHEN @SortType = 'LastDateTime' AND @IsSearchASC = 1 THEN t.LastDateTime END DESC,
-				CASE WHEN @SortType = 'TicketId' AND @IsSearchASC = 0 THEN t.TicketId END ASC,
-				CASE WHEN @SortType = 'Subject' AND @IsSearchASC = 0 THEN t.Subject END ASC,
-				CASE WHEN @SortType = 'Category' AND @IsSearchASC = 0 THEN t.CategoryName END ASC,
-				CASE WHEN @SortType = 'Status' AND @IsSearchASC = 0 THEN t.StateName END ASC,
-				CASE WHEN @SortType = 'SubmitDate' AND @IsSearchASC = 0 THEN t.DateTime END ASC,
-				CASE WHEN @SortType = 'LastDateTime' AND @IsSearchASC = 0 THEN t.LastDateTime END ASC
-				) tt GROUP BY tt.TicketId, tt.Subject, tt.DateTime, tt.CategoryName, tt.StateName, tt.FirstName, tt.LastName, tt.LastDateTime, tt.PriorityName
-					 ORDER BY
-						CASE WHEN @SortType = '' THEN MIN(tt.DateTime) END DESC,
-						CASE WHEN @SortType = 'TicketId' AND @IsSearchASC = 1 THEN tt.TicketId END ASC,
-						CASE WHEN @SortType = 'Subject' AND @IsSearchASC = 1 THEN tt.Subject END ASC,
-						CASE WHEN @SortType = 'Category' AND @IsSearchASC = 1 THEN tt.CategoryName END ASC,
-						CASE WHEN @SortType = 'Status' AND @IsSearchASC = 1 THEN tt.StateName END ASC,
-						CASE WHEN @SortType = 'SubmitDate' AND @IsSearchASC = 1 THEN tt.DateTime END ASC,
-						CASE WHEN @SortType = 'LastDateTime' AND @IsSearchASC = 1 THEN tt.LastDateTime END ASC,
-						CASE WHEN @SortType = 'TicketId' AND @IsSearchASC = 0 THEN tt.TicketId END DESC,
-						CASE WHEN @SortType = 'Subject' AND @IsSearchASC = 0 THEN tt.Subject END DESC,
-						CASE WHEN @SortType = 'Category' AND @IsSearchASC = 0 THEN tt.CategoryName END DESC,
-						CASE WHEN @SortType = 'Status' AND @IsSearchASC = 0 THEN tt.StateName END DESC,
-						CASE WHEN @SortType = 'SubmitDate' AND @IsSearchASC = 0 THEN tt.DateTime END ASC,
-						CASE WHEN @SortType = 'LastDateTime' AND @IsSearchASC = 0 THEN tt.LastDateTime END DESC
+		WITH Tickets AS
+		(
+			SELECT Ticket.TicketId, MAX(Ticket.Subject) AS Subject, MIN(TicketComment.DateTime) AS DateTime, MAX(Category.CategoryName) AS CategoryName, MAX(State.StateName) AS StateName, MAX(FirstName) AS FirstName, MAX(LastName) AS LastName, MAX(TicketComment.DateTime) AS LastDateTime, MAX(Priority.PriorityName) as PriorityName,
+			ROW_NUMBER() OVER
+			(
+				ORDER BY 
+					CASE WHEN @SortType = '' THEN MIN(TicketComment.DateTime) END DESC,
+					CASE WHEN @SortType = 'TicketId' AND @IsSearchASC = 1 THEN Ticket.TicketId END ASC,
+					CASE WHEN @SortType = 'Subject' AND @IsSearchASC = 1 THEN MAX(Ticket.Subject) END ASC,
+					CASE WHEN @SortType = 'Category' AND @IsSearchASC = 1 THEN MAX(CategoryName) END ASC,
+					CASE WHEN @SortType = 'Status' AND @IsSearchASC = 1 THEN MAX(StateName) END ASC,
+					CASE WHEN @SortType = 'SubmitDate' AND @IsSearchASC = 1 THEN MIN(TicketComment.DateTime) END ASC,
+					CASE WHEN @SortType = 'LastDateTime' AND @IsSearchASC = 1 THEN MAX(TicketComment.DateTime) END ASC,
+					CASE WHEN @SortType = 'Priority' AND @IsSearchASC = 1 THEN MAX(PriorityName) END ASC,
+					CASE WHEN @SortType = 'TicketId' AND @IsSearchASC = 0 THEN Ticket.TicketId END DESC,
+					CASE WHEN @SortType = 'Subject' AND @IsSearchASC = 0 THEN MAX(Subject) END DESC,
+					CASE WHEN @SortType = 'Category' AND @IsSearchASC = 0 THEN MAX(CategoryName) END DESC,
+					CASE WHEN @SortType = 'Status' AND @IsSearchASC = 0 THEN MAX(StateName) END DESC,
+					CASE WHEN @SortType = 'SubmitDate' AND @IsSearchASC = 0 THEN MIN(TicketComment.DateTime) END DESC,
+					CASE WHEN @SortType = 'LastDateTime' AND @IsSearchASC = 0 THEN MAX(TicketComment.DateTime) END DESC,
+					CASE WHEN @SortType = 'Priority' AND @IsSearchASC = 0 THEN MAX(PriorityName) END DESC
+			) AS RowNumber
+			FROM Ticket
+				INNER JOIN TicketComment ON Ticket.TicketId = TicketComment.TicketId
+				INNER JOIN State ON Ticket.StateId = State.StateId
+				INNER JOIN Category ON Ticket.CategoryId = Category.CategoryId
+				INNER JOIN UserCategory ON UserCategory.UserId = @UserId AND (UserCategory.CategoryId = Ticket.CategoryId OR UserCategory.CategoryId = Category.ParentCategoryId)
+				LEFT JOIN [User] ON [User].UserId = Ticket.AssignedToUserId
+				INNER JOIN Priority ON Ticket.PriorityId = Priority.PriorityId
+			WHERE (@TextToSearch = '')
+				OR (@SearchType = 'Subject' AND Subject like ('%' + @TextToSearch + '%'))
+				OR (@SearchType = 'Category' AND CategoryName like ('%' + @TextToSearch + '%'))
+				OR (@SearchType = 'Status' AND StateName like ('%' + @TextToSearch + '%')) 
+			GROUP BY Ticket.TicketId
+		)
+		SELECT TicketId, Subject, DateTime, CategoryName, StateName, FirstName, LastName, LastDateTime, PriorityName
+		FROM Tickets
+		WHERE RowNumber BETWEEN ((@RequestedPageNumber-1)*@TicketsPerPage+1) AND (@RequestedPageNumber*@TicketsPerPage)
 
 		-- set param for audit event
 		SELECT @Action = 'Requested assigned tickets; the stored procedure was successfully executed.'
